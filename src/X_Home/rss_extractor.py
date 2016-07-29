c23461f374794c7be098609f5987d78246eb097b
Ankur Mundra <ankurmundra0212@gmail.com>
2016-07-29 12:10:59 +0530

    no more repitition of news articles & much more /Ankur

--- src/X_Home/rss_extractor.py
+++ src/X_Home/rss_extractor.py
@@ -6,9 +6,7 @@ def extractor() :
 	news_feed_title=d['channel']['title']
 	news_feed_image=d['channel']['image']['url']
 	print(d.feed.title)
-	print(d)
-	#webbrowser.open(news_feed_image)
-
+	 # print(d)
 	return d
 
 
--- src/X_Home/views.py
+++ src/X_Home/views.py
@@ -1,39 +1,66 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from .rss_extractor import extractor
 from .models import Articles
+from .forms import contactForm
 
 def home(request):
+    obj = extractor()
+    title_news = obj['channel']['title']
+    image_url = obj['channel']['image']['url']
 
-	title="Hello There !"	
-	obj=extractor()
-	title_news=obj['channel']['title']
-	image_url=obj['channel']['image']['url']
+    asp = Articles.objects.all()[::-1]
+    for news in obj.entries:
+        entry_img = news.media_thumbnail[0]["url"]
+        entry_title = news.title
+        entry_sum = news.summary
+        for ob in asp:
+            if entry_title == ob.title:
+                break
+        else:
+            a = Articles(title=entry_title, summary=entry_sum, img_url=entry_img)
+            a.save()
+            asp = Articles.objects.all()[::-1]
 
-	# for news in obj.entries:
+    asp = Articles.objects.all()[::-1]
+    context = {
+        "ent": asp
+    }
+    return render(request,'home.html',context)
 
-	# 	entry_img=news.media_thumbnail[0]["url"]
-	# 	entry_title=news.title
-	# 	entry_sum=news.summary
+    if request.user.is_authenticated() and request.user.is_staff:
+        # print(signup.objects.all())
+        # i = 1
+        # for instance in signup.objects.all():
+        #   print(i)
+        #   print(instance.Name)
+        #   i += 1
 
-	# 	a = Articles(title=entry_title,summary=entry_sum,img_url=entry_img)
-	# 	a.save()
+        queryset = signup.objects.all().order_by('-timestamp')#.filter(Name__iexact="abc").count()
 
-	asp=Articles.objects.all()[::-1][15:40]
-	print(asp[0].title)	
+        context = {
+            "queryset": queryset
+        }
 
-	print(image_url)
-	context={
+def contact(request):
+    title = "Enter your Details"
+    form = contactForm(request.POST or None)
+    if form.is_valid():
+        #print(form.cleaned_data)
+        form_email = form.cleaned_data.get("Email")
+        form_message = form.cleaned_data.get("Message")
+        form_name = form.cleaned_data.get("Name")
 
-			# "title_context":title,
-			# "title_news":title_news,
-			# "img_link":entry_img,
-			# "entry_title":entry_title,
-			# "entry_sum":entry_sum,
-			"ent":asp
+        subject = 'site contact form'
+        from_email = settings.EMAIL_HOST_USER
+        to_email = [from_email,'your_other_email']
+        contact_message = "%s: %s via %s"%(form_name, form_message, form_email)
 
-			}
+        send_mail(subject, contact_message, from_email, [to_email], fail_silently=False)
 
-	return render(request,"home.html",context)
+
+    context = {
+        "template_title": title,
+        "contact_form": form,
+    }
+
+    return render(request, "forms.html", context)
